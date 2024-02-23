84fdee656d3bc40535dbe0a68470e85bba073fea
Hyoyeon Kim <Sefact@users.noreply.github.com>
2024-02-23 22:22:14 +0900

    Title: 청소당번 정하기, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240223/청소당번 정하기/cleaning-numbering.py
+++ 240223/청소당번 정하기/cleaning-numbering.py
@@ -11,29 +11,56 @@ tempA = 0
 tempB = 0
 tempC = 0
 
-
 for i in range(1, n+1):
-    if i % 2 == 0 and i % 3 == 0:
-        if tempA > tempB:
-            b += 1
-            tempB = i
-        elif tempA < tempB:
+    if i % 2 == 0 and i % 3 == 0 and i % 12 == 0:
+        minVal = tempA
+        if tempB < minVal:
+            minVal = tempB
+        if tempC < minVal:
+            minVal = tempC
+
+        if minVal == tempA:
             a += 1
             tempA = i
+        elif minVal == tempB:
+            b += 1
+            tempB = i
+        elif minVal == tempC:
+            c += 1
+            tempC = i
+    elif i % 2 == 0 and i % 3 == 0:
+        minVal = tempA
+        if tempB < minVal:
+            minVal = tempB
+        
+        if minVal == tempA:
+            a += 1
+            tempA = i
+        elif minVal == tempB:
+            b += 1
+            tempB = i
     elif i % 2 == 0 and i % 12 == 0:
-        if tempA > tempC:
-            c += 1
-            tempC = i
-        elif tempA < tempC:
+        minVal = tempA
+        if tempC < minVal:
+            minVal = tempC
+        
+        if minVal == tempA:
             a += 1
             tempA = i
-    elif i % 3 == 0 and i % 12 == 0:
-        if tempB > tempC:
+        elif minVal == tempC:
             c += 1
             tempC = i
-        elif tempB < tempC:
+    elif i % 3 == 0 and i % 12 == 0:
+        minVal = tempB
+        if tempC < minVal:
+            minVal = tempC
+        
+        if minVal == tempB:
             b += 1
             tempB = i
+        elif minVal == tempC:
+            c += 1
+            tempC = i
     else:
         if i % 2 == 0:
             a += 1
