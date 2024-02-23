a863ece7ad0665722bcf48923d06a27eb9c7db50
Hyoyeon Kim <Sefact@users.noreply.github.com>
2024-02-23 22:01:10 +0900

    Title: 청소당번 정하기, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240223/청소당번 정하기/cleaning-numbering.py
+++ 240223/청소당번 정하기/cleaning-numbering.py
@@ -7,28 +7,42 @@ b = 0
 # 화장실(12일)
 c = 0
 
-for i in range(1, n):
+tempA = 0
+tempB = 0
+tempC = 0
+
+
+for i in range(1, n+1):
     if i % 2 == 0 and i % 3 == 0:
-        if a > b:
+        if tempA > tempB:
             b += 1
-        elif b > a:
+            tempB = i
+        elif tempA < tempB:
             a += 1
+            tempA = i
     elif i % 2 == 0 and i % 12 == 0:
-        if a > c:
+        if tempA > tempC:
             c += 1
-        elif c > a:
+            tempC = i
+        elif tempA < tempC:
             a += 1
+            tempA = i
     elif i % 3 == 0 and i % 12 == 0:
-        if b > c:
+        if tempB > tempC:
             c += 1
-        elif c > b:
+            tempC = i
+        elif tempB < tempC:
             b += 1
+            tempB = i
     else:
         if i % 2 == 0:
             a += 1
+            tempA = i
         elif i % 3 == 0:
             b += 1
+            tempB = i
         elif i % 12 == 0:
             c += 1
+            tempC = i
 
 print(a, b, c)
