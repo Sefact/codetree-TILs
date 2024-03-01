d58601289f13a2098b0c39d709e4c5eb6aa96466
Hyoyeon Kim <Sefact@users.noreply.github.com>
2024-03-01 23:13:05 +0900

    Title: ab 사이에 있는 c, Time: 142ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240301/ab 사이에 있는 c/c-between-a-and-b.py
@@ -0,0 +1,12 @@
+a, b, c = map(int, input().split())
+
+val = False
+for i in range(a, b + 1):
+    if i % c == 0:
+        val = True
+        break
+
+if val:
+    print("YES")
+else:
+    print("NO")
