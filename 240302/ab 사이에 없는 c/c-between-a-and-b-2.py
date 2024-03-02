c13edd2575232e274662f4494db5103e18a023b4
Hyoyeon Kim <Sefact@users.noreply.github.com>
2024-03-02 23:12:07 +0900

    Title: ab 사이에 없는 c, Time: 141ms, Memory: 24MB, Status: Passed - CodeTree

--- 240302/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 240302/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -2,11 +2,11 @@ a, b, c = map(int, input().split())
 val = False
 
 for i in range(a, b + 1):
-    if i % c != 0:
+    if i % c == 0:
         val = True
         break
 
 if val == True:
-    print('YES')
-else:
     print('NO')
+else:
+    print('YES')
